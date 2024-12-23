976b694c26ab32022adac2259b08238917a78531
wjh719 <https://github.com/wjh719>
2024-12-23 15:49:33 +0900

    work6

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -13,6 +13,7 @@ class App {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("==명언 앱 ==");
+        int lastNo =1;
 
         while (true) {
             System.out.print("명령) ");
@@ -28,7 +29,8 @@ class App {
                 System.out.print("작가 : ");
                 scanner.nextLine();
 
-                System.out.println("1번 명언이 등록되었습니다.");
+                System.out.println(lastNo + "번 명언이 등록되었습니다.");
+                lastNo++;
             }
         }
     }
