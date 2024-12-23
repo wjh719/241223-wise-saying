06098659271bd352c08c91d934d2639723c6a18f
wjh719 <https://github.com/wjh719>
2024-12-23 14:49:24 +0900

    work 1 - print

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,6 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("==명언 앱 ==");
+        System.out.println("명령) 종료");
+    }
 }
