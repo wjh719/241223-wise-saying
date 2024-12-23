5cfc398f24ea119a89cb6dca924230698f26f194
wjh719 <https://github.com/wjh719>
2024-12-23 15:34:58 +0900

    work4

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,18 +8,25 @@ public class Main {
 }
 
 class App {
-    public void run () {
+    public void run() {
 
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("==명언 앱 ==");
 
-        System.out.println("명령) 등록");
-        System.out.print("명언 : ");
-        scanner.nextLine(); // 입력값 가져옴. 입력값이 없으면 기다린다. 엔터를 쳐야 입력이 완료됨
-        System.out.print("작가 : ");
-        scanner.nextLine();
-        System.out.println("1번 명언이 등록되었습니다.");
-        System.out.println("명령) 종료");
+        while (true) {
+            System.out.print("명령) ");
+            String command = scanner.nextLine();
+
+            if(command.equals("등록")) {
+                System.out.print("명언 : ");
+                scanner.nextLine(); // 입력값 가져옴. 입력값이 없으면 기다린다. 엔터를 쳐야 입력이 완료됨
+
+                System.out.print("작가 : ");
+                scanner.nextLine();
+
+                System.out.println("1번 명언이 등록되었습니다.");
+            }
+        }
     }
 }
