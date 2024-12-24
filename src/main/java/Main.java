7f029a231bad487c0350548432e3d8c655e01d82
wjh719 <https://github.com/wjh719>
2024-12-24 10:18:30 +0900

    work10

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -16,30 +16,43 @@ class App {
         int lastNo = 0;
         String[] contentList = new String[3];
         String[] authorList = new String[3];
+        WiseSaying wiseSaying = new WiseSaying();
 
-        while(true) {
+        while (true) {
             System.out.print("명령) ");
             String command = scanner.nextLine();
 
-            if(command.equals("종료")) {
+            if (command.equals("종료")) {
                 System.out.println("명언 앱을 종료합니다.");
                 break;
-            } else if(command.equals("등록")) {
+            } else if (command.equals("등록")) {
 
                 System.out.print("명언 : ");
-                contentList[lastNo] = scanner.nextLine(); // 입력값 가져옴. 입력값이 없으면 기다린다. 엔터를 쳐야 입력이 완료됨. 그래야 넘어감
+                String content = scanner.nextLine(); // 입력값 가져옴. 입력값이 없으면 기다린다. 엔터를 쳐야 입력이 완료됨. 그래야 넘어감
 
                 System.out.print("작가 : ");
-                authorList[lastNo] = scanner.nextLine();
+                String author = scanner.nextLine();
 
-                System.out.println("%d번 명언이 등록되었습니다.".formatted(++lastNo));
-            } else if(command.equals("목록")) {
+                wiseSaying.id = ++lastNo;
+                wiseSaying.content = content;
+                wiseSaying.author = author;
+
+                System.out.println("%d번 명언이 등록되었습니다.".formatted(lastNo));
+
+            } else if (command.equals("목록")) {
                 System.out.println("번호 / 작가 / 명언");
                 System.out.println("----------------------");
-                for(int i = 0; i < 3; i++) {
-                System.out.println("%d / %s / %s".formatted(lastNo, authorList[i], contentList[i]));
-            }
+                System.out.println("%d / %s / %s".formatted(wiseSaying.id, wiseSaying.author, wiseSaying.content));
+//                for (int i = 0; i < 3; i++) {
+//                    System.out.println("%d / %s / %s".formatted(lastNo, authorList[i], contentList[i]));
+//                }
             }
         }
+    }
+
+    class WiseSaying {
+        int id;
+        String content;
+        String author;
     }
 }
