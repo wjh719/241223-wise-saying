d002e520291c341edfa765c1ff76af2598f444a1
wjh719 <https://github.com/wjh719>
2024-12-24 09:44:12 +0900

    work9

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -14,8 +14,8 @@ class App {
         Scanner scanner = new Scanner(System.in);
         System.out.println("== 명언 앱 ==");
         int lastNo = 0;
-        String content = "";
-        String author = "";
+        String[] contentList = new String[3];
+        String[] authorList = new String[3];
 
         while(true) {
             System.out.print("명령) ");
@@ -27,16 +27,18 @@ class App {
             } else if(command.equals("등록")) {
 
                 System.out.print("명언 : ");
-                content = scanner.nextLine(); // 입력값 가져옴. 입력값이 없으면 기다린다. 엔터를 쳐야 입력이 완료됨. 그래야 넘어감
+                contentList[lastNo] = scanner.nextLine(); // 입력값 가져옴. 입력값이 없으면 기다린다. 엔터를 쳐야 입력이 완료됨. 그래야 넘어감
 
                 System.out.print("작가 : ");
-                author = scanner.nextLine();
+                authorList[lastNo] = scanner.nextLine();
 
                 System.out.println("%d번 명언이 등록되었습니다.".formatted(++lastNo));
             } else if(command.equals("목록")) {
                 System.out.println("번호 / 작가 / 명언");
                 System.out.println("----------------------");
-                System.out.println("%d / %s / %s".formatted(lastNo, author, content));
+                for(int i = 0; i < 3; i++) {
+                System.out.println("%d / %s / %s".formatted(lastNo, authorList[i], contentList[i]));
+            }
             }
         }
     }
