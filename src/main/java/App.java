4cca82f26a6bbfc78ff266cd27e5c98581db981c
wjh719 <https://github.com/wjh719>
2024-12-24 14:05:40 +0900

    work20

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,19 +1,18 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class App {
-    // 클래스에서 함수들이 공유해야 하는 변수 -> 인스턴스 변수
     private int lastId = 0;
     private int wiseSayingSize = 0;
-    private final WiseSaying[] wiseSayingList = new WiseSaying[3];
+    private final ArrayList<WiseSaying> wiseSayingList = new ArrayList<>();
     Scanner scanner = new Scanner(System.in);
 
     public void run() {
 
         add("꿈을 지녀라. 그러면 어려운 현실을 이길 수 있다.", "월트 디즈니");
+
         System.out.println("== 명언 앱 ==");
-
         while (true) {
-
             System.out.print("명령) ");
             String command = scanner.nextLine();
 
@@ -23,22 +22,18 @@ public class App {
 
             } else if (command.equals("등록")) {
                 writeWiseSaying();
-
             } else if (command.equals("목록")) {
                 printWiseSayingList();
-                }
             }
         }
+    }
 
     private void printWiseSayingList() {
         System.out.println("번호 / 작가 / 명언");
-
         System.out.println("----------------------");
 
-        for (int i = 0; i < wiseSayingSize; i++) {
-
-            WiseSaying wiseSaying = wiseSayingList[i];
-
+        for (int i = 0; i < wiseSayingList.size(); i++) {
+            WiseSaying wiseSaying = wiseSayingList.get(i);
             System.out.println("%d / %s / %s".formatted(wiseSaying.getId(), wiseSaying.getAuthor(), wiseSaying.getContent()));
         }
     }
@@ -56,6 +51,7 @@ public class App {
 
     public void add(String content, String author) {
         WiseSaying wiseSaying = new WiseSaying(++lastId, content, author);
-        wiseSayingList[wiseSayingSize++] = wiseSaying;
+        wiseSayingList.add(wiseSaying);
     }
+
 }
