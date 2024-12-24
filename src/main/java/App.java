df31cd9706fa36e454129e5dee1d006da640f8b4
wjh719 <https://github.com/wjh719>
2024-12-24 12:41:51 +0900

    work19

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,36 +1,59 @@
 import java.util.Scanner;
+
 public class App {
     // 클래스에서 함수들이 공유해야 하는 변수 -> 인스턴스 변수
     private int lastId = 0;
     private int wiseSayingSize = 0;
     private final WiseSaying[] wiseSayingList = new WiseSaying[3];
+    Scanner scanner = new Scanner(System.in);
+
     public void run() {
-        Scanner scanner = new Scanner(System.in);
+
         add("꿈을 지녀라. 그러면 어려운 현실을 이길 수 있다.", "월트 디즈니");
         System.out.println("== 명언 앱 ==");
-        while(true) {
+
+        while (true) {
+
             System.out.print("명령) ");
             String command = scanner.nextLine();
-            if(command.equals("종료")) {
+
+            if (command.equals("종료")) {
                 System.out.println("명언 앱을 종료합니다.");
                 break;
-            } else if(command.equals("등록")) {
-                System.out.print("명언 : ");
-                String content = scanner.nextLine();
-                System.out.print("작가 : ");
-                String author = scanner.nextLine();
-                add(content, author);
-                System.out.println("%d번 명언이 등록되었습니다.".formatted(lastId));
-            } else if(command.equals("목록")) {
-                System.out.println("번호 / 작가 / 명언");
-                System.out.println("----------------------");
-                for(int i = 0; i < wiseSayingSize; i++) {
-                    WiseSaying wiseSaying = wiseSayingList[i];
-                    System.out.println("%d / %s / %s".formatted(wiseSaying.getId(), wiseSaying.getAuthor(), wiseSaying.getContent()));
+
+            } else if (command.equals("등록")) {
+                writeWiseSaying();
+
+            } else if (command.equals("목록")) {
+                printWiseSayingList();
                 }
             }
         }
+
+    private void printWiseSayingList() {
+        System.out.println("번호 / 작가 / 명언");
+
+        System.out.println("----------------------");
+
+        for (int i = 0; i < wiseSayingSize; i++) {
+
+            WiseSaying wiseSaying = wiseSayingList[i];
+
+            System.out.println("%d / %s / %s".formatted(wiseSaying.getId(), wiseSaying.getAuthor(), wiseSaying.getContent()));
+        }
     }
+
+    private void writeWiseSaying() {
+        System.out.print("명언 : ");
+        String content = scanner.nextLine();
+
+        System.out.print("작가 : ");
+        String author = scanner.nextLine();
+
+        add(content, author);
+        System.out.println("%d번 명언이 등록되었습니다.".formatted(lastId));
+    }
+
     public void add(String content, String author) {
         WiseSaying wiseSaying = new WiseSaying(++lastId, content, author);
         wiseSayingList[wiseSayingSize++] = wiseSaying;
